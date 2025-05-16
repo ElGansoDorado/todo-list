a95e16b8549019012a5746c07ae04a696d8425a6
ElCarambola <yakovchikdenis@gmail.com>
2025-05-16 22:49:01 +0300

    added error validation

--- app/(tabs)/create.tsx
+++ app/(tabs)/create.tsx
@@ -1,10 +1,10 @@
 import { SafeAreaView, SafeAreaProvider } from 'react-native-safe-area-context';
-import { StyleSheet, View, Button } from 'react-native';
+import { StyleSheet, View, Button, Text } from 'react-native';
 
 import { useState } from 'react';
 import { Colors } from '@/constants/Colors';
 
-import { CustomInput } from '@/components/ui/CustomInput';
+import { CustomInput } from '@/components/CustomInput';
 import { createTask } from '@/constants/api';
 
 
@@ -13,12 +13,57 @@ export default function CreateScreen() {
   const [date, setDate] = useState('');
   const [text, setText] = useState('');
 
+  const [titleError, setTitleError] = useState('');
+  const [dateError, setDateError] = useState('');
+  const [textError, setTextError] = useState('');
+
+  const clearForm = () => {
+    setTitle('');
+    setDate('');
+    setText('');
+  }
+
+  const clearFormError = () => {
+    setTitleError('');
+    setDateError('');
+    setTextError('');
+  }
+
+  const validation = () => {
+    clearFormError();
+
+    if (title.length <= 0) {
+      setTitleError('the title field is not filled in');
+      return false;
+    }
+
+    if (text.length <= 0) {
+      setTextError('the text field is not filled in');
+      return false;
+    }
+
+    if (date.length <= 0) {
+      setDateError('the date field is not filled in');
+      return false;
+    }
+
+    return true;
+  }
+
+  const created = () => {
+    if (validation()) {
+      createTask({ title, date, text });
+      clearForm();
+    }
+  }
+
   return (
     <View style={styles.container}>
       <SafeAreaProvider>
         <SafeAreaView>
           <View style={styles.box}>
             <CustomInput
+              error={titleError !== ''}
               onChangeValue={setTitle}
               value={title}
               label='Title'
@@ -26,6 +71,7 @@ export default function CreateScreen() {
             />
 
             <CustomInput
+              error={dateError !== ''}
               onChangeValue={setDate}
               value={date}
               label='Date'
@@ -35,14 +81,19 @@ export default function CreateScreen() {
           </View>
 
           <CustomInput
+            error={textError !== ''}
             onChangeValue={setText}
             value={text}
             label='Text'
             placeholder='your task text'
           />
 
-          <Button title="Нажми меня"  onPress={() => createTask({title, date, text})}/>
-
+          <Button title="Нажми меня" onPress={() => created()} />
+          <View style={styles.error}>
+            <Text style={styles.error}>{titleError && titleError}</Text>
+            <Text style={styles.error}>{dateError && dateError}</Text>
+            <Text style={styles.error}>{textError && textError}</Text>
+          </View>
         </SafeAreaView>
       </SafeAreaProvider>
     </View>
@@ -58,7 +109,7 @@ const styles = StyleSheet.create({
     paddingRight: 10,
     paddingTop: 15,
     paddingBottom: 15,
-    
+
     backgroundColor: Colors.light.background,
   },
   box: {
@@ -67,4 +118,8 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     gap: 12,
   },
+  error: {
+    alignItems: 'center',
+    color: 'red',
+  }
 });
---
+++ components/CustomInput.tsx
@@ -0,0 +1,62 @@
+import { View, Text, TextInput, StyleSheet } from "react-native"
+
+import { Colors } from "@/constants/Colors";
+
+type Props = {
+  error: boolean,
+  value: string,
+  onChangeValue: (text: string) => void,
+  label: string,
+  placeholder: string;
+}
+
+export function CustomInput({ error, value, onChangeValue, label, placeholder }: Props) {
+  return <View style={styles.container}>
+    <Text style={styles.label}>{label}</Text>
+    <TextInput
+      style={error ?  styles.inputError : styles.input}
+
+      value={value}
+      onChangeText={onChangeValue}
+      placeholder={placeholder}
+      placeholderTextColor={Colors.light.placeholder} />
+  </View>
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+  },
+  label: {
+    marginLeft: 12,
+    marginBottom: 4,
+
+    fontWeight: "500",
+    fontSize: 16,
+
+    textTransform: 'uppercase',
+    color: '#FFF',
+  },
+  input: {
+    height: 40,
+    padding: 12,
+
+    color: '#FFF',
+    backgroundColor: Colors.light.border,
+
+    borderWidth: 1,
+    borderColor: Colors.light.tint,
+    borderRadius: 22,
+  },
+  inputError: {
+    height: 40,
+    padding: 12,
+
+    color: '#FFF',
+    backgroundColor: Colors.light.border,
+
+    borderWidth: 1,
+    borderColor: 'red',
+    borderRadius: 22,
+  },
+});
